edabbc636b78caf29362ea42ba84a9fab77b144d
Ron Chambers <rechmbrs@gmail.com>
2022-02-12 17:51:11 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+const header =
+    '<h3 class="centerText">' +
+    "by Ron Chambers" +
+    "<br>" +
+    "sch00.01.00a" +
+    "<br>" +
+    "12Feb2022" +
+    "<br>" +
+    "&copy;2022 RONC" +
+    "</h3>";
